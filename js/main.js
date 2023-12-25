208f24794cc50cfc8c77eb6e39e3542b9b2e756a
bigmanabel <bigmanabel@outlook.com>
2023-12-25 02:43:24 +0000

    Fixed contact form

--- js/main.js
+++ js/main.js
@@ -99,3 +99,41 @@
 })(jQuery);
 
 
+
+function sendEmail(event) {
+    event.preventDefault();
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        phone: document.getElementById("phone").value,
+        address: document.getElementById("address").value,
+        message: document.getElementById("message").value
+    };
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(formData),
+        redirect: 'follow'
+    };
+
+    fetch("http://bloody002.pythonanywhere.com/send-email/", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            console.log(result);
+            alert("Your message has been sent successfully!");
+            window.location.href = "thank-you.html";
+        })
+        .catch(error => {
+            console.log('error', error);
+            // Display an error message if there's an issue with sending the email
+            alert("An error occurred while sending your message. Please try again later.");
+        });
+}
+
+document.getElementById("contactForm").addEventListener("submit", sendEmail);
